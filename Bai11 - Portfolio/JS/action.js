77f03df4419f61e0fc1a89c34aceff008633ac1e
cRinstalmaiden <57482536+CrinstalMaiden@users.noreply.github.com>
2022-12-28 17:07:38 +0700

    Add files via upload

--- Bai11 - Portfolio/JS/action.js
+++ Bai11 - Portfolio/JS/action.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
+
+    // Popup
+
+    $(".popup1").hide();
+    $(".message").hide();
+    $(".thankyou").hide();
+
+    $("#hireme").click(function(){
+        $(".popup1").show();
+        $(".message").show();
+    })
+
+    $("#send").click(function(){
+        $(".message").hide();
+        $(".thankyou").show();
+    })
+
+    $("#back").click(function(){
+        $(".popup1").hide();
+        $(".thankyou").hide();
+    })
+
+    $("#close").click(function(){
+        $(".popup1").hide();
+    })
+
+    // Navigation buttons
+
     $("#abtme, #me").click(function(){
         // $(document).scrollTop(1080)
         $('html, body').animate({ scrollTop : $(".aboutme").offset().top}, 500);
@@ -18,5 +46,10 @@ $(document).ready(function(){
 
     $("#contact").click(function(){
         $('html, body').animate({ scrollTop : $(".layer1").offset().top + 4995}, 500);
+    })
+
+    //View all
+    $("#viewall").click(function(){
+        window.open("https://www.behance.net/arena-multimedia?isa0=1", "_blank");
     })
 });
